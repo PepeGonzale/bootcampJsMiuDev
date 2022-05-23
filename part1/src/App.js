0bfaca60a9a17368872b9eafbe69690fc477d612
Pepe Gonzalez <pepeglezdevega@gmail.com>
2022-05-24 00:06:24 +0200

    Primeros pasos

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,13 +1,25 @@
 
 import './App.css';
+import Mensaje from './Mensaje';
 //Asi se crean los componentes, siempre empiezan por mayuscula para no condunfir con elementos de html. 
-function App() {
+const App  = () => {
   return (
     //En realidad es js pero pasa por babeljs y parece HTML pero no lo es. 
-    <div className="App">
-      HOLA MUNDO   
-    </div>
+    //Para que se lean las variables hay que ponerlas entre llaves
+    <div className="App">         
+      <App2/>  
+      <Mensaje color = 'red' message = 'Estamos trabajando '/>
+      <Mensaje color = 'green' message = 'en un curso de React'/>
+    </div>   
   );
 }
-
+//los componenentes se deben crear fuera de otros componentes
+const App2 = () => {
+  const mensaje = 'hola'
+  return (
+    <div className='App2'>     
+        <h2>PROBANDO {mensaje}</h2>
+    </div>  
+  )
+}
 export default App;
---
+++ part1/src/Mensaje.js
@@ -0,0 +1,10 @@
+const Mensaje = (params) =>{
+   
+    return(
+        
+        <h1 style={{color: params.color}}>
+            {params.message}
+            </h1>
+    )
+}
+export default Mensaje;
